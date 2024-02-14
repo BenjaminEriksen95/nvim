17ff9714b553508569b525dee3591fe0f713c47c
Benjamin Eriksen <benja.e95@gmail.com>
2024-02-14 13:39:52 +0100

    packer.lua

--- lua/evilgoldfish/packer.lua
+++ lua/evilgoldfish/packer.lua
@@ -1,4 +1,4 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
+print("Packer installed use :PackerSync to install packages")
 
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
