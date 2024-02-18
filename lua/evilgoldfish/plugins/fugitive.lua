fcfaa15b74d0f51b5e7e83a1473c0006f6f98a31
Benjamin Eriksen <benja.e95@gmail.com>
2024-02-18 11:05:53 +0100

    v:null

--- lua/evilgoldfish/plugins/fugitive.lua
+++ lua/evilgoldfish/plugins/fugitive.lua
@@ -3,8 +3,18 @@ return {
 
     config = function()
         vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-
+        vim.keymap.set("n", "<leader>gA", vim.cmd.Git)
         -- TODO 
+        
+        vim.keymap.set("n", "<leader>gc", function() 
+            local msg = vim.fn.input("Commit message: ")
+            if msg ~= "" then
+                vim.cmd('Git commit -m '..vim.fn.shellescape(message))
+            else
+                print("Commit aborted: No commit message provided")
+            end
+        end, {noremap = true, silent = true})
+
     end
 
 }
