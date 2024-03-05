8c0c6214fd3535a3767b44c5db8db8b5c356563b
Benjamin Eriksen <benja.e95@gmail.com>
2024-03-05 17:50:15 +0100

    add desc

--- lua/evilgoldfish/remap.lua
+++ lua/evilgoldfish/remap.lua
@@ -1,31 +1,24 @@
-print("loading remaps")
-
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = 'Explorer'})
 
 -- Move selected lines up or down
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = 'Move marking Down'})
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = 'Move marking Up'})
 
 -- Down and up with centering
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = 'Move [D]own with centering'})
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = 'Move [D]own with centering'})
+vim.keymap.set("n", "n", "nzzzv", { desc = 'Overwrite [N]ext to center around item'})
+vim.keymap.set("n", "N", "Nzzzv", { desc = 'Overwrite reverse [N]ext to center around item'})
 
--- Pastes over selection without yanking
-vim.keymap.set("x", "<leader>p", [["_dP]])
 
---
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = '[Paste] without yank'})
+
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]], { desc = '[Y]ank selection OS'})
+vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = '[Y]ank line OS'})
 
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
-
-vim.keymap.set("n", "<leader>tb", function ()
-    vim.cmd("Telescope buffers")
-end)
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = '[F]ormat'})
 
