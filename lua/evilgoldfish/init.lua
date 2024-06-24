142b822d703c8e1e8566912a03bdffe481643a5e
Benjamin Eriksen <benja.e95@gmail.com>
2024-06-24 09:48:04 +0200

    feat(keymap): improve keymap and descriptions

--- lua/evilgoldfish/init.lua
+++ lua/evilgoldfish/init.lua
@@ -24,30 +24,36 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
     pattern = "*",
     command = [[%s/\s\+$//e]],
 })
+
 vim.api.nvim_create_autocmd('LspAttach', {
     group = my_group,
     callback = function(e)
         local buffer = e.buf
-        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end,
-            { buffer = buffer, desc = "[G]o to [D]efinition" })
-        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, { buffer = buffer, desc = "Show hover information" })
+        vim.keymap.set("n", "<leader>gd", function() vim.lsp.buf.definition() end,
+            { buffer = buffer, desc = "[g]o to [d]efinition" })
+        vim.keymap.set("n", "<leader>gD", function()
+            vim.cmd("belowright split")
+            vim.lsp.buf.definition()
+        end, { buffer = buffer, desc = "[g]o to [D]efinition in new split" })
+        vim.keymap.set("n", "K", function() vim.lsp.buf.hover()  end,
+            { buffer = buffer, desc = "Show hover information" })
         vim.keymap.set("n", "<leader>ws", function() vim.lsp.buf.workspace_symbol() end,
-            { buffer = buffer, desc = "[W]orkspace [S]ymbols" })
-        vim.keymap.set("n", "<leader>fd", function() vim.diagnostic.open_float() end,
-            { buffer = buffer, desc = "[F]loat [D]iagnostics" })
+            { buffer = buffer, desc = "[w]orkspace [s]ymbols" })
+        vim.keymap.set("n", "<leader>df", function() vim.diagnostic.open_float() end,
+            { buffer = buffer, desc = "[d]iagnostics [f]loat" })
         vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end,
-            { buffer = buffer, silent = true, noremap = true, desc = "[C]ode [A]ctions" })
-        vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.references() end,
-            { buffer = buffer, desc = "[R]efe[r]ences" })
+            { buffer = buffer, silent = true, noremap = true, desc = "[c]ode [a]ctions" })
+        --vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.references() end,
+        --    { buffer = buffer, desc = "[R]efe[r]ences" })
         vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end,
-            { buffer = buffer, desc = "[R]e[n]ame symbol" })
-        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end,
-            { buffer = buffer, desc = "Signature [H]elp" })
-        vim.keymap.set("n", "<leader>d", function() vim.diagnostic.goto_next() end,
-            { buffer = buffer, desc = "Next [D]iagnostic" })
-        vim.keymap.set("n", "<leader>D", function() vim.diagnostic.goto_prev() end,
-            { buffer = buffer, desc = "Previous [D]iagnostic" })
-        vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Diagnostic [Q]uickfix list' })
+            { buffer = buffer, desc = "Re[n]ame symbol" })
+        vim.keymap.set("i", "<C-x>", function() vim.lsp.buf.signature_help() end,
+            { buffer = buffer, desc = "Signature expand" })
+        vim.keymap.set("n", "<leader>ø", function() vim.diagnostic.goto_next() end,
+            { buffer = buffer, desc = "Next diagnostic" })
+        vim.keymap.set("n", "<leader>æ", function() vim.diagnostic.goto_prev() end,
+            { buffer = buffer, desc = "Previous diagnostic" })
+        vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Diagnostic [q]uickfix list' })
     end
 })
 
--- lua/evilgoldfish/remap.lua
+++ lua/evilgoldfish/remap.lua
@@ -5,19 +5,20 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = 'Explorer'})
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = 'Move marking Down'})
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = 'Move marking Up'})
 
--- Down and up with centering
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = 'Move [D]own with centering'})
-vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = 'Move [D]own with centering'})
-vim.keymap.set("n", "n", "nzzzv", { desc = 'Overwrite [N]ext to center around item'})
+-- Down and up with centering - conflicts with hapoon bindings
+-- vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = 'Move [d]own with centering'})
+-- vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = 'Move [u]p with centering'})
+vim.keymap.set("n", "n", "nzzzv", { desc = 'Overwrite [n]ext to center around item'})
 vim.keymap.set("n", "N", "Nzzzv", { desc = 'Overwrite reverse [N]ext to center around item'})
 
 
-vim.keymap.set("x", "<leader>p", [["_dP]], { desc = '[Paste] without yank'})
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = '[p]aste without yank'})
 
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]], { desc = '[Y]ank selection OS'})
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]], { desc = '[y]ank selection OS'})
 vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = '[Y]ank line OS'})
 
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+-- Delete without yank - conflicts with
+--vim.keymap.set({"n", "v"}, "<leader>d"
 
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = '[F]ormat'})
