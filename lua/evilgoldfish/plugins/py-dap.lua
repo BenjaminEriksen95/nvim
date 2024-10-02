8760216a84214ceb6096b74e8d3d99362b81a6c1
Benjamin Eriksen <benja.e95@gmail.com>
2024-10-02 12:35:34 +0200

    feat(bindings): upgrade debugger bindings and desc
    upgrade debugger bindings and desc

--- lua/evilgoldfish/plugins/py-dap.lua
+++ lua/evilgoldfish/plugins/py-dap.lua
@@ -10,20 +10,19 @@ return {
         local dap_py = require('dap-python')
         dap_py.setup(path)
 
-        vim.keymap.set("n", '<leader>dc', vim.cmd.DapContinue)
-
-        vim.keymap.set("n", '<leader>bb', vim.cmd.DapToggleBreakpoint)
-        vim.keymap.set("n", '<leader>bc', "<cmd>lua require'dap'.clear_breakpoints()<cr>")
-        vim.keymap.set("n", '<leader>tc', function()
+        vim.keymap.set("n", '<leader>dc', vim.cmd.DapContinue, {desc="[d]ebugger [c]ontinue"})
+        vim.keymap.set("n", '<leader>db', vim.cmd.DapToggleBreakpoint, {desc="[d]ebugger [b]reakpoint"})
+        vim.keymap.set("n", '<leader>dC', "<cmd>lua require'dap'.clear_breakpoints()<cr>", {desc="[d]ebugger [C]lear"})
+        vim.keymap.set("n", '<leader>dtc', function()
             if vim.bo.filetype == 'python' then
                 dap_py.test_class();
             end
-        end, { desc = "[T]est [C]lass Debug"})
-        vim.keymap.set("n", '<leader>tm', function()
+        end, { desc = "[d]ebug [t]est [c]lass"})
+        vim.keymap.set("n", '<leader>dtm', function()
             if vim.bo.filetype == 'python' then
                 dap_py.test_method();
             end
-        end, { desc = "[T]est [M]ethod Debug"})
+        end, { desc = "[d]ebug [t]est [m]ethod"})
     end
 }
 
